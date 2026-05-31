e1ed504a1bcf88788b6b39a39c62a0e5c2bfe934
Andrew Schmelyun <schmelyun@gmail.com>
2026-05-31 15:27:48 -0400

    Adds composer to build script

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-composer install --no-dev --optimize-autoloader --no-interaction --prefer-dist --working-dir=app
+curl -sS https://getcomposer.org/installer | php -- --quiet
+php composer.phar install --no-dev --optimize-autoloader --no-interaction --prefer-dist --working-dir=app
 
 npm ci
 
